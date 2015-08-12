303be12d06bbff52e8bed77f47bf2b2e293c02b8
vigneshv1989 <viki3110@gmail.com>
2015-08-12 16:44:53 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+maven-project-czp
+This is my maven project from Chez for Advanced Anypoint Studio Online Training.
+
+1. Clone repo - git remote add origin https://github.com/vigneshvp/maven-project-czp.git
+2. Build Project - mvn clean package
+ 
---
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -0,0 +1,24 @@
+package com.mulesoft.training;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.mule.api.MuleEvent;
+import org.mule.tck.junit4.FunctionalTestCase;
+
+// Test snippet: http://bit.ly/1Uz2vEN
+
+
+public class HelloMavenTest extends FunctionalTestCase {
+	
+	@Test
+	public void sayHelloReturnsHelloMaven() throws Exception {
+		runFlowAndExpect("mavenFlow", "Hello World");
+	}
+	
+	@Override
+	protected String getConfigFile() {
+		// TODO Auto-generated method stub
+		return "maven-project.xml";
+	}
+
+}
