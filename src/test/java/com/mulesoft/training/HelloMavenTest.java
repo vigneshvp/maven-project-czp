41998325f074cc87f4ae533f4328234c488ef2d7
vigneshv1989 <viki3110@gmail.com>
2015-08-12 18:40:14 -0400

    Added Property transformer for JSON

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -18,8 +18,8 @@ public class HelloMavenTest extends FunctionalTestCase {
 	@Test
 	public void retrieveFlightsReturnsJson() throws Exception {
 		MuleEvent event = runFlow("retrieveFlights");
-		String contenttype = event.getMessage().getOutboundProperty("Content-Type"); 
-		assertEquals("application/json",contenttype);
+		String contentType = event.getMessage().getOutboundProperty("Content-Type"); 
+		assertEquals("application/json",contentType);
 	}
 	
 	@Override
